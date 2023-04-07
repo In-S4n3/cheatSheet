4f9972b4c17abfb7e22399fab86dd57cd3eac41b
In-S4n3 <tiagodcpp@gmail.com>
2023-04-07 21:56:51 +0100

    added binary search

--- JS-CheatSheet.js
+++ JS-CheatSheet.js
@@ -1270,3 +1270,22 @@ tree.insert(17);
 // Sort the binary search tree using in-order traversal
 console.log(tree.sort());
 // result => [2, 5, 7, 10, 12, 15, 17]
+
+//----------------------------------------------------------------
+// Binary search
+
+function binSearch(arr, toSearch) {
+  // TODO
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.floor((start + end) / 2);
+
+  while (start <= end) {
+    if (toSearch < arr[middle]) end = middle - 1;
+    else start = middle + 1;
+
+    middle = Math.floor((start + end) / 2);
+  }
+
+  return arr[middle] === toSearch ? middle : -1;
+}
